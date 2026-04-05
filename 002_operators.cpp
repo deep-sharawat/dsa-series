b0b07c51eb6eca5491baec2f52db02d641954203
Deepanshu Sharawat <deepanshu6sharawat@gmail.com>
2026-04-05 14:28:45 +0530

    added operators file

---
+++ 002_operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 12;
+    int b = 6;
+
+    cout <<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a/b<<endl;
+    cout<<a*b<<endl;
+    cout<<a%b<<endl;  //modulo operator gives remainder as output
+}
