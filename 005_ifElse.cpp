1df82b166e9d070e5a01a9209df345fa8783a924
Deepanshu Sharawat <deepanshu6sharawat@gmail.com>
2026-04-05 23:15:54 +0530

    if else if added in 005

--- 005_ifElse.cpp
+++ 005_ifElse.cpp
@@ -5,13 +5,24 @@ int main(){
     int a ,b;
     cin >>a>>b;
 
-    if (a>b){
-        cout << "a is greater than b"<<endl;
-    }else{
-        if (a<b){
-            cout<<"a is less than b"<<endl;
-        }else{
-            cout<<"a and b are equal";
-        }
-    }
+//     if (a>b){
+//         cout << "a is greater than b"<<endl;
+//     }else{
+//         if (a<b){
+//             cout<<"a is less than b"<<endl;
+//         }else{
+//             cout<<"a and b are equal";
+//         }
+//     }
+// }   uper wala is bad method
+
+if (a>b){
+    cout<<"a is greater than b";
+}
+else if (a<b){
+    cout<<"a is less than b";
+}
+else if(a==b){
+    cout<<"a is equal to b"; 
+}
 }
