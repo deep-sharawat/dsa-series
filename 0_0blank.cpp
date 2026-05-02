b6d0e566eb6da230e23a79a58031c15d1de4acbf
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-02 21:46:44 +0530

    hehe

--- 0_0blank.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout <<"hello world";  //no enter 
-    cout <<"hii1"<<endl;   //new line character
-    cout <<"hii2"<<'\n';   //new line character
-    cout <<"abc\n";        //new line character
-}
