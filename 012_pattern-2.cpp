7ed8d046cc4ba76c38edec703d06f58111c16af2
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-03 21:42:38 +0530

    added pattern 2 file named as 012;

--- 012_pattern-2.cpp
+++ 012_pattern-2.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
-int main (){
+int main(){
     int n;
     cin>>n;
     int i = 1;
-    while(i<=n){
-        int j =1;
+
+    while (i<=n){
+        int j = 1;
         while(j<=n){
-            cout<<i<<" ";
+            cout << i<<" ";
             j = j+1;
         }
         cout<<endl;
-        i = i+1;
+        i = i +1;
+
     }
 }
