cf956272242977f1cea8abf0b1a44b5c6da65134
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-03 17:32:40 +0530

    added 8 ,9 and 10

---
+++ 010_farenheitTOcelcius.cpp
@@ -0,0 +1,24 @@
+// #include <iostream> 
+// using namespace std; 
+// int main(){
+//     float f;
+//     cin>>f;
+
+//     float c;
+//     c = (5*(f-32))/9;
+//     cout<<c;
+// }
+
+//or use 
+#include <iostream> 
+using namespace std; 
+int main(){
+    float f;
+    cin>>f;
+
+    float c;
+    c = (5.0/9)*(f-32);
+    cout<<c;
+}
+
+//1st one is better then second one and dont use 5/9. use 5.0/9
