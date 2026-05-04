4324e5c5df496378a04b9b72797331f0804320be
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-04 12:07:02 +0530

    added 021 file

---
+++ 021_pattern-11.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    char first = 'A';
+
+    while(i<=n){
+        int j= 1;
+        while(j<=n){
+            cout <<first;
+           
+            j = j+1;
+        }cout<<endl;
+        i = i+1;
+         first = first+1;
+    }
+}
+
+
+// //by formula 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i = 1;
+//     char first = 'A';
+    
+//     while(i<=n){
+//         int j= 1;
+//         char b = first +i-1;
+//         while(j<=n){
+//             cout <<b;
+           
+//             j = j+1;
+//         }cout<<endl;
+//         i = i+1;
+         
+//     }
+// }
