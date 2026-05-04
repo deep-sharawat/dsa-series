2f959d290c4787f6deb4405c6fe98a77368ebaf1
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-04 11:56:42 +0530

    added 020 file

---
+++ 020_pattern-10.cpp
@@ -0,0 +1,40 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i = 1;
+    
+    
+//     while(i<=n){
+//         int j= 1;
+//         int row = i;
+//         while(j<=i){
+//             cout <<row;
+//             row = row-1;
+//             j = j+1;
+//         }cout<<endl;
+//         i = i+1;
+//     }
+// }
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    
+    
+    while(i<=n){
+        int j= 1;
+        
+        while(j<=i){
+            cout <<i-j+1;
+            
+            j = j+1;
+        }cout<<endl;
+        i = i+1;
+    }
+}
