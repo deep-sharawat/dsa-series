50c1b91a5a6054ff99be302342124ee2b0326f36
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-04 14:23:20 +0530

    added 025 file

---
+++ 025_pattern-15.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    char val= 'A';
+    while(i<=n){
+        int j= 1;
+        
+        while(j<=i){ 
+            
+            cout <<val<<" ";
+            
+            j = j+1;
+        }cout<<endl;
+        i = i+1;
+        val =val+1;
+    }
+}
