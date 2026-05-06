53c6e977451d9dae50e3987121365cdfdec665ac
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-06 21:23:05 +0530

    added ncr file

---
+++ 045_nCr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n){
+    int ans = 1;
+    for (int i = 1; i<=n;i++){
+        ans = ans * i;
+    }
+    return ans;
+}
+
+int nCr(int p , int q){
+
+    return fact(p)/(fact(q)*fact(p-q));
+
+}
+int main(){
+    int a,b;
+    cout<<"enter the value to ncr";
+    cin>>a>>b;
+    cout<<nCr(a,b);
+}
