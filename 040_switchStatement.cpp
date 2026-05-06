03aafa650a4b34a2575a3b261b0ed75f2fb33073
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-06 17:46:55 +0530

    added 040 switch stmt

---
+++ 040_switchStatement.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int n = 2;
+    
+    cout<<endl;
+    switch(n){
+        
+        case 1 :  cout<<"first"<<endl;
+            break;
+
+        case 2 : cout<<"second"<<endl;
+            break;
+
+        default : cout<<"its a defalut case "<<endl;
+
+    }
+    cout<<endl;
+
+
+}
