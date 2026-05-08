e5b48b566e0c37d91efc592b17208759e1b2c905
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-08 20:37:29 +0530

    added 050

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,39 +2,39 @@
 using namespace std;
 
 int getMax(int array[] , int size1){
-   int max = INT_MIN;
+
+   int a = INT_MIN;
+
    for(int i = 0; i<size1;i++){
-      if(array[i]>max){
-         max = array[i];
-      }
-   }return max;
+        a = max(array[i],a);
+   }return a;
 
 }
 int getMin(int array2[],int size2){
-   int min = INT_MAX;
+   int b = INT_MAX;
    for(int i = 0; i<size2;i++){
-      if(array2[i]<min){
-         min = array2[i];
-      }
-   }return min;
+
+      b = min(array2[i],b);
+      
+   }return b;
 }
 
 int main(){
-   int arr[1000];
+   int arr[100];
    int size;
    cout<<"enter size : ";
    cin>>size;
 
    for (int i =0; i<size; i++){
-      cout<<"enter "<<i+1<<"value : ";
+      cout<<"enter "<<i+1<<"value : "<<endl;
       cin>>arr[i];
 
 
    }
          int g = getMax(arr , size);
-      cout<<"maxium value is : "<<g;
+      cout<<"maxium value is : "<<g<<endl;
 
       int h = getMin(arr , size);
-      cout<<"minimum value is : "<<h;
+      cout<<"minimum value is : "<<h<<endl;
 
 }
