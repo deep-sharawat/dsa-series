93eff0a96c773ace51e3468288b1a35266113b43
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-08 20:26:40 +0530

    049  and 050 updated

--- 049_minMax.cpp
+++ 049_minMax.cpp
@@ -1,33 +1,40 @@
 #include<iostream>
 using namespace std;
+
+int getMax(int array[] , int size1){
+   int max = INT_MIN;
+   for(int i = 0; i<size1;i++){
+      if(array[i]>max){
+         max = array[i];
+      }
+   }return max;
+
+}
+int getMin(int array2[],int size2){
+   int min = INT_MAX;
+   for(int i = 0; i<size2;i++){
+      if(array2[i]<min){
+         min = array2[i];
+      }
+   }return min;
+}
+
 int main(){
+   int arr[1000];
+   int size;
+   cout<<"enter size : ";
+   cin>>size;
 
- int arr[100];
+   for (int i =0; i<size; i++){
+      cout<<"enter "<<i+1<<"value : ";
+      cin>>arr[i];
 
-    int size;
-    cout<<"enter the size of array :";
-    cin>>size;
 
-       
-    for(int i =0;i<size;i++){
-        cout<<"enter value : ";
-        cin>>arr[i];
+   }
+         int g = getMax(arr , size);
+      cout<<"maxium value is : "<<g;
 
-    }
-         int a = INT_MIN;
-
-         for(int i =0;i<size;i++){
-            if (arr[i]>a){
-                a = arr[i];
-            }
-         }cout<<"max :"<<a<<endl;
-
-            int v = INT_MAX;
-         for (int i = 0; i<size;i++){
-                if (arr[i]<v){
-                     v = arr[i];
-                }
-         }cout<<"min value is : "<<v;
-
+      int h = getMin(arr , size);
+      cout<<"minimum value is : "<<h;
 
 }
