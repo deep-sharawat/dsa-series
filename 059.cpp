37bd5c59397ecf3c567eb79d0795d9ba876cbcc7
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-11 22:27:41 +0530

    added temp file and updated 059

--- 059.cpp
+++ 059.cpp
@@ -1,5 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main(){
-    
+
+    int size;
+    cout<<"enter size : ";
+    cin>>size;
+
+    int i = 0;
+    while (i<size){
+        int j = 0;
+        while (j<i){
+            cout<<"* ";
+            j++;
+        }cout<<endl;
+        i++;
+    }
 }
