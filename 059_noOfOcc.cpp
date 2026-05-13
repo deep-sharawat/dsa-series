2f8b292ae6787bed60a9771982bd1781ce81d8e4
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-13 11:49:08 +0530

    added 060

---
+++ 059_noOfOcc.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int occ(int arr[], int size, int key){
+    int start = 0;
+    int end = size -1;
+    int number = 0;
+    int mid = start - (end -start)/2;
+
+    while (start <= end){
+        if (arr[mid] == )
+    }
+}
+
+int main(){
+
+        int array[7] = {1,2,3,3,3,3,9};
+
+}
